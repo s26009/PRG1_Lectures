4adfa8623392335322d347abc0bcf8f868e1bae8
Hleb Skachko <s26009@pjwstk.edu.pl>
2021-10-18 22:11:04 +0200

    added Time::operators, reworked time storage

--- lecture_3/src/include/s1234/Time.h
+++ lecture_3/src/include/s1234/Time.h
@@ -1,15 +1,28 @@
 #include <string>
 
+enum class Time_of_day { MORNING, DAY, EVENING, NIGHT };
+
 struct Time {
     int hour;
     int minute;
     int second;
+	long seconds; 
 
 	std::string to_string();
+	std::string to_string(Time_of_day tod);
+	Time_of_day time_of_day();
 	void next_hour();
 	void next_minute();
 	void next_second();
 	void refactor();
 	Time(int index, int semester, int rate);
-};
+	Time(long seconds);
+	Time(){};
 
+	Time operator+ (Time);
+	Time operator- (Time);
+	bool operator< (Time);
+	bool operator> (Time);
+	bool operator== (Time);
+	bool operator!= (Time);
+};
--- lecture_3/src/s03-Time.cpp
+++ lecture_3/src/s03-Time.cpp
@@ -4,15 +4,19 @@
 #include "include/s1234/Time.h"
 
 Time::Time(int hour_value, 
-	int minute_value, int second_value) :
-	hour(hour_value), minute(minute_value), second(second_value) {
+	int minute_value, int second_value) {
+		seconds = second_value + minute_value * 60 + hour_value * 3600;
+		refactor();
+	}
+
+Time::Time(long seconds_value) : seconds(seconds_value) {
 		refactor();
 	}
 
 std::ostream& operator << (std::ostream &os, const Time &s) {
     return (os << s.hour 
 			<< ":" << s.minute 
-			<< ":" << s.second  << "\n");
+			<< ":" << s.second);
 }
 
 std::string Time::to_string() {
@@ -21,48 +25,100 @@ std::string Time::to_string() {
     return ss.str();
 }
 
-void Time::refactor() {
-	if(this->hour > 23) { 
-		hour -= 24;
-		refactor();
-	}
-	if(this->minute > 59) {
-		minute -= 60;
-		hour++;
-		refactor();
-	}
-	if(this->second > 59) {
-		second -= 60;
-		minute++;
-		refactor();
+std::string Time::to_string(Time_of_day tod) {
+	switch(tod) {
+		case Time_of_day::MORNING: return "Morning";
+		case Time_of_day::DAY: return "Day";
+		case Time_of_day::EVENING: return "Evening";
+		case Time_of_day::NIGHT: return "Night";
+		default: return "the end of the world";
 	}
 }
 
+void Time::refactor() {
+	if(seconds >= 86400) {
+		seconds -= 86400;
+	}
+	if(seconds < 0) {
+		seconds += 86400;
+	}
+	hour = seconds / 3600;
+	minute = (seconds % 3600) / 60;
+	second = (seconds % 3600) % 60;
+}
+
 void Time::next_hour() {
-	this->hour++;
+	seconds += 3600;
 	refactor();
 }
 
 void Time::next_minute() {
-	this->minute++;
+	seconds += 60;
 	refactor();
 }
 
 void Time::next_second() {
-	this->second++;
+	seconds++;
 	refactor();
 }
 
-int main() {
-    Time* time = new Time(19, 234, 359);
-	std::cout << time->to_string();
+Time Time::operator+(Time time2) {
+	return Time(seconds + time2.seconds);
+}
 
-	time->next_hour();
-	std::cout << "next_hour()\n" << time->to_string();
-	time->next_minute();
-	std::cout << "next_minute()\n" << time->to_string();
-	time->next_second();
-	std::cout << "next_second()\n" << time->to_string();
+Time Time::operator-(Time time2) {
+	return Time(seconds - time2.seconds);
+}
+
+bool Time::operator>(Time time2) {
+	return seconds > time2.seconds;
+}
+
+bool Time::operator<(Time time2) {
+	return seconds < time2.seconds;
+}
+
+bool Time::operator==(Time time2) {
+	return seconds == time2.seconds;
+}
+
+bool Time::operator!=(Time time2) {
+	return seconds != time2.seconds;
+}
+
+int main() {
+    Time time1 = Time(19, 234, 359);
+    Time time2 = Time(15, 50, 15);
+	std::cout << time1.to_string();
+
+	time1.next_hour();
+	std::cout << "\nnext_hour()\n" << time1.to_string();
+	time1.next_minute();
+	std::cout << "\nnext_minute()\n" << time1.to_string();
+	time1.next_second();
+	std::cout << "\nnext_second()\n" << time1.to_string();
+	std::cout << "\n" << time1.to_string(Time_of_day::MORNING) << "\n";
+
+	Time time3 = time1 + time2;
+	std::cout << time1.to_string() << " + " << time2.to_string() <<
+	" = " << time3.to_string() << "\n";
+	
+	time3 = time1 - time2;
+	std::cout << time1.to_string() << " - " << time2.to_string() <<
+	" = " << time3.to_string() << "\n";
+
+	std::cout << time1.to_string() << " < " << time2.to_string() <<
+	" = " << (time1 < time2) << "\n";
+
+	std::cout << time1.to_string() << " > " << time2.to_string() <<
+	" = " << (time1 > time2) << "\n";
+
+	std::cout << time1.to_string() << " == " << time2.to_string() <<
+	" = " << (time1 == time2) << "\n";
+		
+	std::cout << time1.to_string() << " != " << time2.to_string() <<
+	" = " << (time1 != time2) << "\n";
+	
     return 0;
 }
 
