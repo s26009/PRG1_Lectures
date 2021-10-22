0f3529bdbf2f0bef6d01444a8551e187e15ade3d
Hleb Skachko <s26009@pjwstk.edu.pl>
2021-10-22 13:00:08 +0200

    added job-queue task

--- lecture_4/src/s05-job-queue.cpp
+++ lecture_4/src/s05-job-queue.cpp
@@ -2,24 +2,38 @@
 #include <chrono>
 #include <thread>
 #include <pthread.h>
+#include <cstring>
+#include <string>
+#include "include/safe_queue.h"
 
-void print_hello(int count) {
-	std::cout << "Hello, " << count << "\n";
+void print(int id, SafeQueue<std::string>& itemQueue) {
+	//std::cout << itemQueue.empty() << " ";
+	while(!itemQueue.empty()) {
+		std::cout << "from thread " << id <<  " : " << itemQueue.dequeue() << "\n";
+		//I'm not sure why, but without this sleep program stucks:		
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));	
+	}
+}
+
+void fillQueue(SafeQueue<std::string>& queue) {
+	int count = 50;
+	while(count --> 0)
+		queue.enqueue("Element #" + std::to_string(count));
 }
 
 auto main(int argc, char* argv[]) -> int
 {
-	int count = 42;
-	
-	while(count --> 0){
-		std::thread thread1(print_hello, count);
-		thread1.join();
-		//Didn't fully get this part: 
-		//Wątki mają być uruchamiane grupami po 6 naraz.
-		//Did this, it kind of separates groups by little pause:
-		if(count % 6 == 0)
-			std::this_thread::sleep_for(std::chrono::milliseconds(400));
-	}
-
+	SafeQueue<std::string> itemQueue;
+    fillQueue(itemQueue);
+    std::thread t1(print, 1, std::ref(itemQueue));
+    std::thread t2(print, 2, std::ref(itemQueue));
+    std::thread t3(print, 3, std::ref(itemQueue));
+    std::thread t4(print, 4, std::ref(itemQueue));
+	t1.detach();
+	t2.detach();	
+	t3.detach();
+	t4.detach();
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::cout << "Finished.\n";
     return 0;
 }
