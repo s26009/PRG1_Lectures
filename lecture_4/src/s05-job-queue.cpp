73f6a644ac7e08a089facca44d048daadd87e751
Hleb Skachko <s26009@pjwstk.edu.pl>
2021-10-29 08:52:44 +0200

    did job queue task, not finished

--- lecture_4/src/s05-job-queue.cpp
+++ lecture_4/src/s05-job-queue.cpp
@@ -4,35 +4,42 @@
 #include <pthread.h>
 #include <cstring>
 #include <string>
-#include "include/safe_queue.h"
+#include <mutex>
+#include <queue>
 
-void print(int id, SafeQueue<std::string>& itemQueue) {
-	//std::cout << itemQueue.empty() << " ";
-	while(!itemQueue.empty()) {
-		std::cout << "from thread " << id <<  " : " << itemQueue.dequeue() << "\n";
-		//I'm not sure why, but without this sleep program stucks:		
-		std::this_thread::sleep_for(std::chrono::milliseconds(20));	
+std::mutex m;
+
+void fillQueue(std::queue<std::string>& queue) {
+	int count = 50;
+	while(count --> 0)
+		queue.push("Element #" + std::to_string(count));
+}
+
+void print(int id, std::queue<std::string>& queue) {
+		m.lock();
+		std::cout << queue.front()  << " poped by thread #" << id << "\n";
+		queue.pop();
+		m.unlock();
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+}
+
+void showAllElements(int id, std::queue<std::string>& queue) {
+	while(!queue.empty()) {
+		print(id, queue);
 	}
 }
 
-void fillQueue(SafeQueue<std::string>& queue) {
-	int count = 50;
-	while(count --> 0)
-		queue.enqueue("Element #" + std::to_string(count));
-}
-
-auto main(int argc, char* argv[]) -> int
-{
-	SafeQueue<std::string> itemQueue;
-    fillQueue(itemQueue);
-    std::thread t1(print, 1, std::ref(itemQueue));
-    std::thread t2(print, 2, std::ref(itemQueue));
-    std::thread t3(print, 3, std::ref(itemQueue));
-    std::thread t4(print, 4, std::ref(itemQueue));
-	t1.detach();
-	t2.detach();	
-	t3.detach();
-	t4.detach();
+auto main(int argc, char* argv[]) -> int {
+	std::queue<std::string> commonQueue;
+    fillQueue(commonQueue);
+	std::thread t1(showAllElements, 1, std::ref(commonQueue));
+    std::thread t2(showAllElements, 2, std::ref(commonQueue));
+    std::thread t3(showAllElements, 3, std::ref(commonQueue));
+    std::thread t4(showAllElements, 4, std::ref(commonQueue));
+	t2.join();
+	t1.join();	
+	t3.join();
+	t4.join();
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 	std::cout << "Finished.\n";
     return 0;
